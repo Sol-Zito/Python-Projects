47b67a3f7639e9d11f5fd4466548250b2798ca11
Sol <solzito.1889@gmail.com>
2024-01-05 17:54:31 -0300

    se agrega validacion

--- Projects/AdministradordeRecetas.py
+++ Projects/AdministradordeRecetas.py
@@ -1,6 +1,7 @@
 # # crea nuevo directory
 import os
 from pathlib import Path
+
 #
 # #La opción 1 le va a preguntar qué categoría elige (carnes, ensaladas, etc.), y una vez que
 # #el usuario elija una, le va a preguntar qué receta quiere leer, y mostrar su contenido.
@@ -10,12 +11,14 @@ directorio_path = Path("C:\\Users\\solzi\\IdeaProjects\\PythonProyect\\Projects\
 carpetas = os.chdir(directorio_path)
 carpetas = os.listdir(carpetas)
 
+
 def validando_eleccion(lista):
     eleccion = ''
     while eleccion not in lista:
         eleccion = input(f"Estas son nuestras opciones, \nElija una: {lista}: ")
     else:
         return eleccion
+
 
 def recorrer_carpetas():
     recetas_total = []
@@ -25,11 +28,13 @@ def recorrer_carpetas():
         recetas_total.append(cant_recetas)
     return recetas_total
 
+
 # # Recorre las recetas segun la carpeta que eligio el usuario
 def recorrer_lista(eleccion):
     cant_recetas = Path(directorio_path, eleccion)
     cant_recetas = os.listdir(cant_recetas)
     return cant_recetas
+
 
 # # Agarra la lista y le saca la extension
 def tranformacion_lista(lista):
@@ -41,6 +46,7 @@ def tranformacion_lista(lista):
         listan.append(sintxt)
     return listan
 
+
 # # Obtencion de receta
 
 def obtencion_receta(carpeta, menu):
@@ -48,10 +54,11 @@ def obtencion_receta(carpeta, menu):
     receta = Path(directorio_path, carpeta, menu)
     print(receta.read_text())
 
+
 # obtencion_receta(recetaElegida)
 
-#En la opción 2 también se le va a hacer elegir una categoría, pero luego le va a pedir que
-#escriba el nombre y el contenido de la nueva receta que quiere crear, y el programa va a crear ese archivo en el lugar correcto.
+# En la opción 2 también se le va a hacer elegir una categoría, pero luego le va a pedir que
+# escriba el nombre y el contenido de la nueva receta que quiere crear, y el programa va a crear ese archivo en el lugar correcto.
 
 def nueva_receta():
     print(f"La receta puede ser de: {carpetas}")
@@ -81,15 +88,16 @@ def crear_categoria():
         else:
             isValida = True
     else:
-        #ruta_carpeta = os.chdir(Path(directorio_path))
+        # ruta_carpeta = os.chdir(Path(directorio_path))
         Path.mkdir(nueva_cat)
-        #carpeta = os.makedirs(nueva_cat)
+        # carpeta = os.makedirs(nueva_cat)
         print("Se creo nueva categoria")
+
 
 # La opción 4, hará lo mismo que la opción uno, pero en vez de leer la receta, la va
 # a eliminar
 
-def borrar_receta(carpeta,menu):
+def borrar_receta(carpeta, menu):
     menu = f'{menu}.txt'
     receta = Path(Path(directorio_path, carpeta, menu))
     Path(receta).unlink()
@@ -105,15 +113,24 @@ def borrar_categoria(categoria):
 # Finalmente, la opción 6 simplemente va a finalizar la ejecución del código.
 
 def inicio():
+    opcionesValidas = ['1', '2', '3', '4', '5', '6']
+
     print("Menu:\n"
           "[1] - Leer receta\n[2] - Crear receta nueva\n[3] - Crear categoria nueva\n"
           "[4] - Eliminar receta\n[5] - Eliminar categoria\n[6] - Salir del programa")
 
-    opcion = input("Ingrese su el nro de su opcion: ")
-    return opcion
+    opcion = ''
+    while opcion not in opcionesValidas:
+        print("Ingrese una opcion valida")
+        opcion = input("Ingrese su el nro de su opcion: ")
+
+    else:
+        return opcion
+
 
 def volverInicio():
     print("Volver:a inicio\n")
+
 
 def cocinaConmigo():
     nombreUser = input("Dime tu nombre: ")
@@ -148,7 +165,6 @@ def cocinaConmigo():
             volverInicio()
         case "6":
             print("Finalizar programa")
-        case _:
-            print("Ingrese una opcion valida")
+
 
 cocinaConmigo()
