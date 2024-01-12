e2fe08db331e28eb84da5fcca772e81a155b1c74
Sol <solzito.1889@gmail.com>
2024-01-12 19:11:43 -0300

    se agregan sonidos y musica de fondo. se agrega funciones para el puntaje. se agrega fin del juego -falta que muestre texto en pantalla-

--- Projects/Dia10/main.py
+++ Projects/Dia10/main.py
@@ -5,6 +5,7 @@ from Bala import *
 from Jugador import *
 from Enemigo import *
 import pygame
+from pygame import mixer
 
 # INICIAR GAME
 pygame.init()
@@ -12,32 +13,31 @@ pygame.init()
 # CREAR PANTALLA
 pantalla = pygame.display.set_mode((800, 600))
 
-
 # TITULO E ICONO
 pygame.display.set_caption('Invasion Espacial')
 icono = pygame.image.load("Photos/ovni.png")
 pygame.display.set_icon(icono)
 fondo = pygame.image.load("Photos/Fondo.jpg")
 
+# AGREGAR MUSICA
+mixer.music.load("Music/MusicaFondo.mp3")
+mixer.music.set_volume(0.4)
+mixer.music.play(-1)
+
 # JUGADOR VARIABLES
 nave = Jugador("Photos/cohete.png", 368, 500)
 jugador_x_cambio = 0
 
-
 # ENEMIGO VARIABLES
-# nave_enemiga = Enemigo("Photos/enemigo.png", random.randint(5, 736), random.randint(50, 200))
-enemigo_x_cambio = 0.3
-enemigo_y_cambio = 50
-
 lista_enemigos = []
 
-for i in range(4):
-    i = Enemigo("Photos/enemigo.png", random.randint(5, 736), random.randint(50, 200))
+for i in range(8):
+    i = Enemigo("Photos/enemigo.png", random.randint(0, 736), random.randint(50, 200), 0.5, 50)
     lista_enemigos.append(i)
-
 
 # BALA VARIABLES
 bala = Bala("Photos/bala.png")
+balas = []
 
 
 # DETECTAR COLISIONES
@@ -50,13 +50,37 @@ def hay_colision(x1, x2, y1, y2):
     else:
         return False
 
+
+# PUNTAJE
+puntaje = 0
+fuente = pygame.font.Font('Fonts/Consider.otf', 32)
+texto_x = 10
+texto_y = 10
+
+
+# MOSTRAR PUNTAJE
+def mostar_puntaje():
+    texto = fuente.render(f"Puntaje: {puntaje}", True, (255, 255, 255))
+    pantalla.blit(texto, (10, 10))
+
+
+# TEXTO FINAL DEL JUEGO
+txt_final = pygame.font.Font('Fonts/Consider.otf', 50)
+
+
+def texto_final():
+    texto_final = txt_final.render("Juego terminado", True, (255, 255, 255))
+    print("Juego terminado")
+    pantalla.blit(texto_final, (60, 200))
+
+
 # LOOP DEL GAME
 se_ejecuta = True
-puntaje = 0
 
 while se_ejecuta:
     # PANTALLA
     pantalla.blit(fondo, (0, 0))
+
     # ITERAR EVENTOS
     for event in pygame.event.get():
         # SIRVE PARA CERRAR EL PROGRAMA
@@ -69,7 +93,10 @@ while se_ejecuta:
                 jugador_x_cambio = -0.2
             if event.key == pygame.K_RIGHT:
                 jugador_x_cambio = 0.2
+
             if event.key == pygame.K_SPACE:
+                sonido_disparo = mixer.Sound('Music/disparo.mp3')
+                sonido_disparo.play()
                 if not bala.isVisible:
                     bala.posicion_x = nave.posicion_x
                     bala.disparo(pantalla)
@@ -79,36 +106,42 @@ while se_ejecuta:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 jugador_x_cambio = 0
 
-
     # MODIFICAR UBICACION JUGADOR
     nave.setPosition(jugador_x_cambio)
 
-
     for e in lista_enemigos:
+        # FIN DEL JUEGO
+        dead = hay_colision(e.posicion_x, nave.posicion_x, e.posicion_y, nave.posicion_y)
+        if dead:
+            lista_enemigos.clear()
+            texto_final()
+            break
+
         # MODIFICAR UBICACION ENEMIGO
-        e.setPosition_x(enemigo_x_cambio)
+        e.setPosition_x(e.posicion_x_cambio)
 
         # MANTENER ENEMIGO DENTRO DE PANTALLA
         if e.posicion_x <= 0:
-            enemigo_x_cambio = 0.2
-            e.posicion_y += enemigo_y_cambio
-        elif e.posicion_x >= 736:
-            enemigo_x_cambio = -0.2
-            e.posicion_y += enemigo_y_cambio
+            e.posicion_x_cambio = 0.3
+            e.posicion_y += e.posicion_y_cambio
+        elif e.posicion_x >= 706:
+            e.posicion_x_cambio = -0.3
+            e.posicion_y += e.posicion_y_cambio
 
         # Colision
-        colision = hay_colision(e.posicion_x, bala.posicion_x, e.posicion_y, bala.posicion_y)
-        if colision:
+        shot = hay_colision(e.posicion_x, bala.posicion_x, e.posicion_y, bala.posicion_y)
+
+        if shot and bala.isVisible:
+            sonido_colision = mixer.Sound("Music/Golpe.mp3")
+            sonido_colision.play()
             bala.posicion_y = 500
             bala.isVisible = False
             puntaje += 1
             print(f"Puntaje: {puntaje}")
-            e.setPosition_x(random.randint(0, 700))
-            e.setPosition_y(random.randint(50, 500))
-            print(f"Nave enemiga: x: {e.posicion_x}, y: {e.posicion_y}")
+            e.setPosition_x(random.randint(0, 706))
+            e.setPosition_y(random.randint(50, 200))
 
         e.enemigo(pantalla)
-
 
     # MANTENER DENTRO DE PANTALLA
     if bala.posicion_y <= -64:
@@ -120,7 +153,7 @@ while se_ejecuta:
 
     nave.jugador(pantalla)
 
+    mostar_puntaje()
 
     # ACTUALIZAR
     pygame.display.update()
-
