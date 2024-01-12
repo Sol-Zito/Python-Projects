a477d7dbd8dcce285b957e4d5bc1e87fac0af25d
Sol <solzito.1889@gmail.com>
2024-01-12 19:10:23 -0300

    se corrige metodo para que el jugador se mantenga en la pantalla

--- Projects/Dia10/Jugador.py
+++ Projects/Dia10/Jugador.py
@@ -13,9 +13,9 @@ class Jugador:
         self.posicion_x += mod_position
         # MANTENER DENTRO DE PANTALLA
         if self.posicion_x <= 0:
-            jugador_x = 0
+            self.posicion_x = 0
         elif self.posicion_x >= 736:
-            jugador_x = 736
+            self.posicion_x = 736
 
     def jugador(self, pantalla):
         pantalla.blit(self.cohete, (self.posicion_x, self.posicion_y))
