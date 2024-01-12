dc6c0ad9448b8fdbb2bfc440a34374fc359b02fe
Sol <solzito.1889@gmail.com>
2024-01-12 19:09:27 -0300

    se agregan 2 atributos al enemigo

--- Projects/Dia10/Enemigo.py
+++ Projects/Dia10/Enemigo.py
@@ -3,10 +3,12 @@ import pygame
 
 class Enemigo:
 
-    def __init__(self, image, x, y):
+    def __init__(self, image, x, y, cambio_x, cambio_y):
         self.forma = pygame.image.load(image)
         self.posicion_x = x
         self.posicion_y = y
+        self.posicion_x_cambio = cambio_x
+        self.posicion_y_cambio = cambio_y
 
 
     def enemigo(self, pantalla):
